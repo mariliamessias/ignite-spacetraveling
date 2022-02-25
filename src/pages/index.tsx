d98865fbe87252f10cd44480f15a2a5a49e875e6
mariliamessias <marilia.oliveira@outlook.com>
2022-02-24 23:34:15 -0300

    add connection with prismic api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
-
+import Prismic from '@prismicio/client';
+import Link from "next/link";
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +25,63 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <main className={styles.containter}>
+        <div className={styles.posts}>
+          {postsPagination.results.map(post => (
+            <Link key={post.uid} href={`/posts/${post.uid}`}>
+              <a key={post.uid}>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <section>
+                  <time>{post.first_publication_date}</time>
+                  <span>{post.data.author}</span>
+                </section>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['publication.title', 'publication.subtitle'],
+      pageSize: 100,
+    }
+  );
+
+  const results = response.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(
+        post.last_publication_date
+      ).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        results,
+      },
+    },
+  };
+};
